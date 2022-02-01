76686392882b2b6ea391222195de5144701d7b35
D-a-Johnson <johnson.a.derek.94@gmail.com>
2022-02-01 11:16:40 +0100

    added uuid for random ids

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import ApplicantList from './ApplicantList';
 
 function App() {
-  const [applicants, setApplicants] = useState([{id: 1, name: 'Derek', job: 'workswise',stage: 1}])
+  const [applicants, setApplicants] = useState([])
+  const applicantName = useRef()
+  const applicantJob = useRef()
+  //{id: 1, name: 'Derek', job: 'workswise',stage: 1}
+
+  function handleAddApplicant(n,j) {
+    const name = applicantName.current.value
+    const job = applicantJob.current.value
+    if ((name === '') || (job === '')) return
+    setApplicants(prevApplicants => {
+      return [...prevApplicants, {id: 1, name: name, job: job,stage: 1}]
+    })
+    applicantName.current.value = null
+    applicantJob.current.value = null
+  }
+
   return (
     <>
     <ApplicantList applicants = {applicants}/>
-    <input type="text" />
-    <button>Add Applicant</button>
+    Name <input ref={applicantName} type="text" /> <br/>
+    Job <input ref={applicantJob} type="text" /> <br/>
+    <button onClick={handleAddApplicant}>Add Applicant</button>
     <button>Clear Applicant</button>
     <div>0 Applicants</div>
     </>
--- src/Applicant.js
+++ src/Applicant.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Applicant({ applicant }) {
   return (
     <div>
-        {applicant.name} - {applicant.job}
+        <label>
+        {applicant.name} - {applicant.job} <br/>
+        <button>Advance</button><button>Delete</button>
+        </label>
     </div>
   )
 }
